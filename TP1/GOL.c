6f47e32e8b3e40f733f200f9d4ebba95dacd14e1
Santiago Wirzt <swirzt@gmail.com>
2020-04-28 17:41:26 -0300

    Mejoradinho

--- TP1/GOL.c
+++ TP1/GOL.c
@@ -1,20 +1,17 @@
 #include "Game.h"
 
-game_t* jueguito;
-
 char* transformaFinal(char* entrada) {
   int largo = strlen(entrada);
   char* final = malloc(sizeof(char) * (largo + 2));
   strncpy(final, entrada, largo - 4);
   final[largo - 4] = '\0';
   strcat(final, "final");
-  printf("%s\n", final);
   return final;
 }
 
 int main(int argc, char* argv[]) {
   int cantHilos = get_nprocs();
-  jueguito = loadGame(argv[1]);
+  game_t* jueguito = loadGame(argv[1]);
   board_t tablero = congwayGoL(jueguito, cantHilos);
   char* guardado = transformaFinal(argv[1]);
   writeBoard(tablero, guardado);
