5008c7fd38ba59e24cc439fb0f6ff2d7cece773a
Santiago Wirzt <swirzt@gmail.com>
2020-06-02 16:58:39 -0300

    Ejercicio semanal

--- Erlang/ej.erl
+++ Erlang/ej.erl
@@ -1,11 +1,31 @@
 -module(ej).
 
--export([w/0, s/0]).
+-export([w/0, s/1]).
 
 w() ->
-  io:format("Trabajando...~n"),
+  io:format("Trabajando en ~p~n", [node()]),
   timer:sleep(2000),
   w().
 
 
-s() -> spawn(?MODULE, w, []).
+keeper([]) ->
+  receive
+    {'EXIT', P, _} ->
+      io:format("El nodo ~p, se desconectó~n", [node(P)]),
+      io:format("No quedan más nodos, terminé~n")
+  end;
+
+keeper([Nname | Ns]) ->
+  receive
+    {'EXIT', P, _} ->
+      io:format("El nodo ~p, se desconectó~n", [node(P)]),
+      io:format("Continuo el trabajo en ~p~n", [Nname]),
+      spawn_link(Nname, ?MODULE, w, [])
+  end,
+  keeper(Ns).
+
+
+s([Nname | Ns]) ->
+  process_flag(trap_exit, true),
+  spawn_link(Nname, ?MODULE, w, []),
+  keeper(Ns).
